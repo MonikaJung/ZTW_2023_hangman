93fd33fddda7b232d15d7bb218818ceed9489fce
MonikaJung <m.nika.jung@gmail.com>
2023-06-22 18:39:01 +0200

    Added dynamic category name

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("main_page.html")
+    return render_template("main_page.html", category="category name")
 
 if __name__ == '__main__':
     app.run()
