12778657ed90ae5d306d4b2f0e15df9b91851a34
MonikaJung <m.nika.jung@gmail.com>
2023-06-22 19:19:55 +0200

    Added hangman images and mistakes count to display

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+MAX_MISTAKES_EASY: int = 13
+CATEGORY_NAME: str = "category name"
+
+current_mistakes: int = 0
+
 @app.route('/')
 def index():
-    return render_template("main_page.html", category="category name")
+    return render_template("main_page.html", category=CATEGORY_NAME, mistakes=current_mistakes, max_mistakes=MAX_MISTAKES_EASY)
+
+def choose_letter():
+    return render_template("main_page.html", category=CATEGORY_NAME, mistakes=current_mistakes, max_mistakes=MAX_MISTAKES_EASY)
 
 if __name__ == '__main__':
     app.run()
